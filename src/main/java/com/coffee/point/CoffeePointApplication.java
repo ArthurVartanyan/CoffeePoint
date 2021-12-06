f5486e85ab9f718266a3903667f0cd61601d10e2
Rogue06 <rogue.2019@icloud.com>
2021-12-06 18:11:48 +0300

    Dependencies for back-end(Java) with Gradle

---
+++ src/main/java/com/coffee/point/CoffeePointApplication.java
@@ -0,0 +1,12 @@
+package com.coffee.point;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeePointApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoffeePointApplication.class, args);
+    }
+}
---
+++ src/test/java/com/coffee/point/CoffeePointApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coffee.point;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeePointApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
